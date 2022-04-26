dd51ae275f62fe6c9156a5e541f3bb2501759f7e
YahyaMohammadIbrahim <92376549+YahyaMohammadIbrahim@users.noreply.github.com>
2022-04-26 14:39:20 +0300

    Add files via upload

--- src/company/company.jsx
+++ src/company/company.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid';
+import CompanyMenu from './menu';
+
 
 export default function company() {
   return (
-    <div>company</div>
+    <div><Grid container spacing={3}>
+    <Grid item xs={3}>
+      <CompanyMenu/>
+    </Grid>
+    <Grid item xs={9}>
+      DDDD
+    </Grid>
+  </Grid>
+  </div>
   )
 }
